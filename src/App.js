8c288becc7c8dc7f7f6ae515d0dbd7d8d9e19358
Alex20511998 <Alex-2504@ukr.net>
2022-04-20 12:43:15 +0300

    hw_5.1

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import {useReducer} from "react";
+import {createRef, useReducer} from "react";
 import Cat from "./components/Cat";
 import Dog from "./components/Dog";
 
@@ -11,52 +11,82 @@ const reducer = (state, action) => {
         case 'addCat':
             return {...state, cats: [...state.cats, {name: payload, id: Date.now()}]}
         case 'delCat':
-            return {...state, cats:state.cats.filter(cat => cat.id !== payload)}
+            return {...state, cats: state.cats.filter(cat => cat.id !== payload)}
         case 'addDog':
             return {...state, dogs: [...state.dogs, {name: payload, id: Date.now()}]}
         case 'delDog':
-            return {...state, dogs:state.dogs.filter(dog => dog.id !== payload)}
+            return {...state, dogs: state.dogs.filter(dog => dog.id !== payload)}
     }
 }
 
 function App() {
     const [state, dispach] = useReducer(reducer, {cats: [], dogs: []})
 
+    const onSubmit = (e) => {
+        console.log(e.target.cat.value);
+        console.log(e.target.dog.value);
+        e.preventDefault();
+    }
+
     const createCat = (e) => {
         e.preventDefault();
-        dispach({type: 'addCat', payload: e.target[0].value});
-        console.log(e.target[0].value);
+        let cat = document.getElementById('cat');
+        dispach({type: 'addCat', payload: cat.value});
+        console.log(cat.value);
+        cat.value = '';
+
     }
 
     const createDog = (e) => {
         e.preventDefault();
-        dispach({type: 'addDog', payload: e.target[0].value});
-        console.log(e.target[0].value);
+        let dog = document.getElementById('dog');
+        dispach({type: 'addDog', payload: dog.value});
+        console.log(dog.value);
+        dog.value = '';
+
     }
 
 
     return (
-        <div className={'header'}>
-            <div>
-                <form onSubmit={createCat}>
-                    <label>AddCat: <input type={'text'}/>
-                        <button>save</button>
+        <div>
+            <form>
+                <div className={'header'}>
+                    <label>AddCat: <input type={'text'} id="cat"/>
+                        <button onClick={createCat}>save</button>
+                        {state.cats.map(cat => <Cat key={cat.id} cat={cat} dispach={dispach}/>)}
                     </label>
-                </form>
-                <div>
-                    {state.cats.map(cat => <Cat key={cat.id} cat={cat} dispach={dispach}/>)}
-                </div>
-            </div>
-            <div>
-                <form onSubmit={createDog}>
-                    <label>AddDog: <input type={'text'}/>
-                        <button>save</button>
+
+                    <label>AddDog: <input type={'text'} id="dog"/>
+                        <button onClick={createDog}>save</button>
+                        {state.dogs.map(dog => <Dog key={dog.id} dog={dog} dispach={dispach}/>)}
                     </label>
-                </form>
-                <div>
-                    {state.dogs.map(dog => <Dog key={dog.id} dog={dog} dispach={dispach}/>)}
+
                 </div>
-            </div>
+
+
+            </form>
+
+
+            {/*<div>*/}
+            {/*    <form onSubmit={createCat}>*/}
+            {/*        <label>AddCat: <input type={'text'}/>*/}
+            {/*            <button>save</button>*/}
+            {/*        </label>*/}
+            {/*    </form>*/}
+            {/*    <div>*/}
+            {/*        {state.cats.map(cat => <Cat key={cat.id} cat={cat} dispach={dispach}/>)}*/}
+            {/*    </div>*/}
+            {/*</div>*/}
+            {/*<div>*/}
+            {/*    <form onSubmit={createDog}>*/}
+            {/*        <label>AddDog: <input type={'text'}/>*/}
+            {/*            <button>save</button>*/}
+            {/*        </label>*/}
+            {/*    </form>*/}
+            {/*    <div>*/}
+            {/*        {state.dogs.map(dog => <Dog key={dog.id} dog={dog} dispach={dispach}/>)}*/}
+            {/*    </div>*/}
+            {/*</div>*/}
 
         </div>
     );
